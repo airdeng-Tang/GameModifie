35bb0472e130d139ec38935781c4c7c4cc48a864
airdeng-Tang <1174141181@qq.com>
2023-05-28 12:31:33 +0800

    首次提交, 获取句柄

---
+++ GameRunningDbg/Model/ProcessModel.cs
@@ -0,0 +1,27 @@
+﻿using GameRunningDbg.Core;
+using GameRunningDbg.Tool;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameRunningDbg.Model
+{
+    public class ProcessModel : Singleton<ProcessModel>
+    {
+        public string name;
+        public Process module;
+        bool isRunning = false;
+
+        public void Init()
+        {
+            isRunning = true;
+        }
+
+        //public process() { 
+            
+        //}
+    }
+}
---
+++ GameRunningDbg/Program.cs
@@ -0,0 +1,9 @@
+﻿
+public class Program
+{
+    private static void Main()
+    {
+        // See https://aka.ms/new-console-template for more information
+        Console.WriteLine("Hello, World!");
+    }
+}
